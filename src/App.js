b93e0073b7531f9ca924681179177e3092ef92f6
Ashwini Kumar Pandey <itsashwani001@gmail.com>
2023-05-14 16:29:28 +0530

    added fetch api linking words api

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import Notification from "./components/Notification";
 import Popup from "./components/Popup";
-import { showNotification as show, checkWin } from "./helpers/helpers";
+import { showNotification as show } from "./helpers/helpers";
 
-const words = ["application", "programming", "interface", "wizard"];
+const words = ["application", "programming", "interface", "wizard", "jazzy", "injury", "apple", "staff", "cricket", "football"];
+
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 
 function App() {
+  // const [words, setWord] = useState('');
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
   const [showNotification, setShowNotification] = useState(false)
+  // useEffect(()=>{
+  //   fetch('https://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=8000&maxCorpusCount=-1&minDictionaryCount=3&maxDictionaryCount=-1&minLength=6&maxLength=12&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
+  //     .then(response => response.json())
+  //     .then(data => setWord(data.words))
+  // })
 
   useEffect(() => {
     const handleKeydown = event => {
